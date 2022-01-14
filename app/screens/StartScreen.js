1464ed3c72da567b6ea7f9133dc54ac429556998
TorstenHaas <torsten.haas@hs-ruhrwest.de>
2022-01-14 22:49:21 +0100

    Updated Formatting for StartScreen

--- app/screens/StartScreen.js
+++ app/screens/StartScreen.js
@@ -1,93 +1,81 @@
-import React from 'react'
+import React from "react";
 import {
-    StyleSheet, 
-    Text, 
-    View, 
-    Image, 
-    StatusBar, 
-    Button, 
-    Alert, 
-    TouchableOpacity
-} from 'react-native'
-import { useNavigationn, NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  StatusBar,
+  Button,
+  Alert,
+  TouchableOpacity,
+} from "react-native";
+import { useNavigationn, NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 export default function StartScreen({ navigation }) {
-  
-    return (
-      
-        
-
+  return (
     <View style={styles.container}>
-    <StatusBar style="auto"/>
-    <Text style={styles.content}>Produktsucher</Text>
-       <Image source = {
-        require("../assets/icon.png")} style={styles.logo}/>
-    
-        <Text style={styles.content}>
-        {"\n"}
-        </Text>
-        <TouchableOpacity
-        onPress={()=> console.log("test")}
-        activeOpacity={.8}
+      <StatusBar style="auto" />
+      <Text style={styles.content}>Produktsucher</Text>
+      <Image source={require("../assets/icon.png")} style={styles.logo} />
+
+      <Text style={styles.content}>{"\n"}</Text>
+      <TouchableOpacity
+        onPress={() => console.log("test")}
+        activeOpacity={0.8}
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Anmelden</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
-       onPress={()=> navigation.navigate('Register')}
-        activeOpacity={.8}
-        style={styles.buttons}>
-
+        onPress={() => navigation.navigate("Register")}
+        activeOpacity={0.8}
+        style={styles.buttons}
+      >
         <Text style={styles.buttonText}>Registrieren</Text>
       </TouchableOpacity>
 
       <TouchableOpacity
-        onPress={()=> navigation.navigate('Impressum')}
-        activeOpacity={.8}
+        onPress={() => navigation.navigate("Impressum")}
+        activeOpacity={0.8}
         style={styles.buttons}
       >
         <Text style={styles.buttonText}>Impressum</Text>
       </TouchableOpacity>
-        
     </View>
-  
-    
-       
-    )
-    
+  );
 }
 
-const styles = StyleSheet.create({  container: {
+const styles = StyleSheet.create({
+  container: {
     flex: 1,
-    backgroundColor: '#FBFCFF',
+    backgroundColor: "#FBFCFF",
     justifyContent: "center",
     alignItems: "center",
   },
 
   content: {
-    marginLeft:10,
+    marginLeft: 10,
     marginRight: 10,
   },
 
-  logo:{
-    height:300,
-    width:"70%",
-   resizeMode: "contain",
+  logo: {
+    height: 300,
+    width: "70%",
+    resizeMode: "contain",
   },
-  buttons:{
-    width:"70%",
-    height:50,
-    margin:10,
+  buttons: {
+    width: "70%",
+    height: 50,
+    margin: 10,
     alignItems: "center",
     backgroundColor: "#274156",
-    borderColor:'rgba(0,0,0,0.2)',
-    borderRadius:20,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderRadius: 20,
   },
-  buttonText:{
-    textAlignVertical:"center",
-    color: 'white',
-    padding:15
+  buttonText: {
+    textAlignVertical: "center",
+    color: "white",
+    padding: 15,
   },
-
-  })
+});
